e881dca139dbc5cbb4e2d651a385873d95bf435b
ziad ehab ahmed <84890422+ziadehab74@users.noreply.github.com>
2024-03-05 14:52:16 +0200

    liveiwre update home

--- app/Http/Livewire/TotalInvoices.php
+++ app/Http/Livewire/TotalInvoices.php
@@ -4,16 +4,51 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\invoices;
+use App\Models\sections;
 
 class TotalInvoices extends Component
 {
     public $invoices_count;
     public $Total_invoices;
+    public $Total_unpaid_invoices;
+    public $unpaid_invoices;
+    public $Total_paid_invoices;
+    public $paid_invoices;
+    public $Total_Partially_invoices;
+    public $Partially_paid_invoices;
+    public $chartjs;
+    public $Partially_paid_invoices_percntage;
+    public $paid_Percntage_invoices;
+    public $unpaid_Percntage_invoices;
     public function count()
     {
+        $this->paid_invoices = invoices::where('Value_Status', '1')->count();
+        $this->paid_Percntage_invoices = $this->paid_invoices  * 100;
+        $this->Total_paid_invoices = number_format(invoices::where('Value_Status', '1')->sum('Total'), 2);
+
+
+        $this->unpaid_invoices = invoices::where('Value_Status', '2')->count();
+        $this->unpaid_Percntage_invoices = $this->unpaid_invoices * 100;
+        $this->Total_unpaid_invoices = number_format(invoices::where('Value_Status', '2')->sum('Total'), 2);
+
+
+        $this->Partially_paid_invoices = invoices::where('Value_Status', '3')->count();
+        $this->Partially_paid_invoices_percntage = $this->Partially_paid_invoices * 100;
+        $this->Total_Partially_invoices = number_format(invoices::where('Value_Status', '3')->sum('Total'), 2);
+
+
+        // $products = products::count();
+        // $this->sections = sections::count();
+
+        $this->invoices_count = invoices::count();
+        $this->Total_invoices = number_format(invoices::sum('Total'), 2);
+
         $this->Total_invoices = number_format(invoices::sum('Total'), 2);
         $this->invoices_count = invoices::count();
+
+
     }
+
     public function render()
     {
         $this->count();
--- resources/views/livewire/total-invoices.blade.php
+++ resources/views/livewire/total-invoices.blade.php
@@ -1,5 +1,92 @@
-
-                        <div class="" wire:poll.1000ms="count">
+<div class="row row-sm" wire:poll.10000ms>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+        <div class="card overflow-hidden sales-card bg-primary-gradient">
+            <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
+                <div class="">
+                    <h6 class="mb-3 tx-18 text-white">إجمالي الفواتير</h6>
+                </div>
+                <div class="pb-0 mt-0">
+                    <div class="d-flex">
+                        <div class="">
                             <h4 class="tx-20 font-weight-bold mb-1 text-white">{{$Total_invoices}}</h4>
                             <p class="mb-0 tx-12 text-white op-7">{{$invoices_count}}</p>
                         </div>
+                        <span class="float-right my-auto mr-auto">
+                            <i class="fas fa-arrow-circle-up text-white"></i>
+                            <span class="text-white op-7"> </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {{-- <span id="compositeline" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span> --}}
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+        <div class="card overflow-hidden sales-card bg-danger-gradient">
+            <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
+                <div class="">
+                    <h6 class="mb-3 tx-18 text-white"> الفواتير غير المدفوعة</h6>
+                </div>
+                <div class="pb-0 mt-0">
+                    <div class="d-flex">
+                        <div class="">
+                            <h4 class="tx-20 font-weight-bold mb-1 text-white">{{$Total_unpaid_invoices}}</h4>
+                            <p class="mb-0 tx-12 text-white op-7">{{$unpaid_invoices}}</p>
+                        </div>
+                        <span class="float-right my-auto mr-auto">
+                            <i class="fas fa-arrow-circle-down text-white"></i>
+                            <span class="text-white op-7">
+
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {{-- <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span> --}}
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+        <div class="card overflow-hidden sales-card bg-success-gradient">
+            <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
+                <div class="">
+                <h6 class="mb-3 tx-18 text-white"> الفواتير المدفوعة</h6>
+                </div>
+                <div class="pb-0 mt-0">
+                    <div class="d-flex">
+                        <div class="">
+                            <h4 class="tx-20 font-weight-bold mb-1 text-white">{{$Total_paid_invoices}}</h4>
+                            <p class="mb-0 tx-12 text-white op-7">{{$paid_invoices}}</p>
+                        </div>
+                        <span class="float-right my-auto mr-auto">
+                            <i class="fas fa-arrow-circle-up text-white"></i>
+                            <span class="text-white op-7"> </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {{-- <span id="compositeline3" class="pt-1">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span> --}}
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+        <div class="card overflow-hidden sales-card bg-warning-gradient">
+            <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
+                <div class="">
+                <h6 class="mb-3 tx-18 text-white"> الفواتيرالمدفوعة جزئيا</h6>
+                </div>
+                <div class="pb-0 mt-0">
+                    <div class="d-flex">
+                        <div class="">
+                            <h4 class="tx-20 font-weight-bold mb-1 text-white">{{$Total_Partially_invoices}}</h4>
+                            <p class="mb-0 tx-12 text-white op-7">{{$Partially_paid_invoices}}</p>
+                        </div>
+                        <span class="float-right my-auto mr-auto">
+                            <i class="fas fa-arrow-circle-down text-white"></i>
+                            <span class="text-white op-7"></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {{-- <span id="compositeline4" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span> --}}
+        </div>
+    </div>
+</div>
